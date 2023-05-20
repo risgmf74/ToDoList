f65a1ca166d9b03a6a038b2f18a7c553a5b17e64
chantawat <chantawat90@gmail.com>
2023-05-20 20:58:39 +0800

    1st commit: completed 1st versions of app

---
+++ date.js
@@ -0,0 +1,15 @@
+// Generate current date
+
+exports.getDate = function() {
+
+    const today = new Date();
+
+    const options = { 
+        weekday: 'long', 
+        month: 'long', 
+        day: 'numeric'
+    };
+
+    return today.toLocaleDateString('en-US', options);
+    
+}
